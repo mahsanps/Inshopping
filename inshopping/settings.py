51f3928eadc7ffee21fd2a76e32b513ad7a4035a
mahsanps <mahsan.ps@gmail.com>
2024-10-07 14:11:50 +0330

    new

--- inshopping/settings.py
+++ inshopping/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-IS_LOCAL = config("IS_LOCAL", default=True, cast=bool)
+IS_LOCAL = config("IS_LOCAL", default=False, cast=bool)
 
 # Authentication model
 LOGIN_URL = "/login/"
