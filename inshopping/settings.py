836b5fe0919ce75156beb167c3cdb08cf1f91381
mahsanps <mahsan.ps@gmail.com>
2024-10-19 16:43:46 +0330

    change-settings-static

--- inshopping/settings.py
+++ inshopping/settings.py
@@ -178,7 +178,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-   
+    os.path.join(BASE_DIR, "statics"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
