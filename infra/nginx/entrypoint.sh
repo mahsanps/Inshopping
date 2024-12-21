bce148a741728f91600d8f2cd7467a9d4c2d80d9
mahsanps <mahsan.ps@gmail.com>
2024-12-21 15:35:34 +0330

    updated nginx dockerfile

--- infra/nginx/entrypoint.sh
+++ infra/nginx/entrypoint.sh
@@ -6,5 +6,4 @@ set -e
 # Wait for Django to be ready
 wait-for-it.sh django:8000 --timeout=60 --strict -- echo "Django is up"
 
-# Start Nginx in the foreground
-exec nginx -g "daemon off;"
+exec "$@"
