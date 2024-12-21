9f66a4b614879b5667eee326cb10a9e21374f138
mahsanps <mahsan.ps@gmail.com>
2024-12-21 14:21:16 +0330

    another fix

--- infra/django/entrypoint.sh
+++ infra/django/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Wait for PostgreSQL database to be ready
-/usr/local/bin/scripts/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
+# Wait for the PostgreSQL database to be ready
+wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
 
-# Optionally wait for Redis if needed
-# /usr/local/bin/scripts/wait-for-it.sh redis:6379 --timeout=60 --strict -- echo "Redis is up"
+# Optionally, wait for Redis if your application uses it
+# wait-for-it.sh redis:6379 --timeout=60 --strict -- echo "Redis is up"
 
 # Start Django server
 exec "$@"
