b79c02e3ac01b91ff9114cbeb79f7c861663ec4e
mahsanps <inshoppingir@ircln20.ihglobaldns.com>
2024-12-23 12:37:58 +0330

    data

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,9 @@
-import sys
+import imp
 import os
+import sys
 
-# Add the project directory to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-# Activate your virtual environment
-activate_this = os.path.join(os.getcwd(), 'venv', 'bin', 'activate_this.py')
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+sys.path.insert(0, os.path.dirname(__file__))
 
-# Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings'
-
-# Import Django's WSGI handler
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+wsgi = imp.load_source('wsgi', 'inshopping/wsgi.py')
+application = wsgi.application 
